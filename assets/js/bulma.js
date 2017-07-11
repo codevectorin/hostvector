a41d2cc5b55f7d568a59661083381537912f18a5
mobihack <aaduthoma19118@gmail.com>
2017-07-11 17:49:09 +0530

    Added flickity based footer svg icons!

--- assets/js/bulma.js
+++ assets/js/bulma.js
@@ -1,22 +1,13 @@
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-(function() {
-    var burger = document.querySelector('.nav-toggle');
-    var menu = document.querySelector('.nav-menu');
-    if (burger!=null) {
-        burger.addEventListener('click', function() {
-            burger.classList.toggle('is-active');
-            menu.classList.toggle('is-active');
-        });
-    }
-})();
-document.addEventListener('DOMContentLoaded', function () {
-        var simple = document.querySelector('.js_slider');
-
-        lory(simple, {
-            infinite: 1
-        });
-    });
+ (function() {
+     var burger = document.querySelector('.navbar-burger');
+     var menu = document.querySelector('.navbar-menu');
+     burger.addEventListener('click', function() {
+         burger.classList.toggle('is-active');
+         menu.classList.toggle('is-active');
+     });
+ })();
 
 //#3273dc
 
